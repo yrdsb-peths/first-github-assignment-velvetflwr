e4be9b4626571297e772237c1ccce9e22dcd3c1f
michelle <166725918+velvetflwr@users.noreply.github.com>
2024-04-16 19:19:53 -0400

    ariana grande.java

---
+++ ariana grande.java
@@ -0,0 +1,15 @@
+public class ArianaGrande
+  {
+    int grammys
+
+    public String ArianaGrande(int grammys)
+    {
+      this.grammys = grammys
+        }
+
+public int recieveAwards()
+    {
+      return grammys;
+    }
+
+  }
